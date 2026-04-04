526f18e1993e12927ec1528b7cd0416fcf2d4f1d
moshe.shimanovich <moshe.shimanovich@lucysecurity.com>
2026-04-04 13:34:42 +0300

    Update volumes and force installation of vendors on boot

--- backend/docker-start.sh
+++ backend/docker-start.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
-#Todo check if needed.
-#needs_composer_install=0
-if [ ! -f vendor/autoload.php ]; then
-#	needs_composer_install=1
-#else
-#	php -r "require 'vendor/autoload.php'; exit(class_exists('Laravel\\Reverb\\ReverbServiceProvider') ? 0 : 1);" || needs_composer_install=1
-#fi
-#
-#if [ "$needs_composer_install" -eq 1 ]; then
-	composer install --no-interaction --no-progress
-fi
-
-if [ ! -d node_modules ]; then
-	npm install
-fi
+# Always make sure composer is installed on boot.
+# This is to make the first run smoother so no need to run composer install manually.
+composer install --no-interaction --no-progress
 
 # Ensure APP_KEY is set before running framework commands that rely on encryption.
 if [ -f .env ]; then
--- frontend/docker-start.sh
+++ frontend/docker-start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
-if [ ! -d node_modules ]; then
-	npm install
-fi
+# Always make sure npm is installed on boot.
+# This is to make the first run smoother so no need to run npm install manually.
+npm install
 
 npm run dev -- --host 0.0.0.0 --port 5173
