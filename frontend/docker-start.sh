f0563419c9d3c5dd7861e26a98dc523722346870
moshe.shimanovich <moshe.shimanovich@lucysecurity.com>
2026-04-04 13:17:14 +0300

    Remove check empty vendor before starting download

--- frontend/docker-start.sh
+++ frontend/docker-start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-if [ ! -d node_modules ]; then
-	npm install
-fi
+# Here to ensure node_modules is always installed on start so no need to run npm installed manually.
+npm install
 
 npm run dev -- --host 0.0.0.0 --port 5173
